a549dc75e3866e81d13fdc1d08e674d66ee59217
Serhii Kuznets <testopesto7@gmail.com>
2024-07-26 23:40:28 +0200

    added marcp

--- scriprt.js
+++ scriprt.js
@@ -5,7 +5,7 @@ const menuTablet = document.querySelector(".menu-tablet");
 const navLinks = document.querySelectorAll(".js-nav-link");
 const navList = document.querySelector(".nav-list");
 const btnOrder = document.querySelector(".order-project-btn");
-
+const body = document.body
 // console.log(navBar);
 // console.log(header);
 // console.log(hamburger)
@@ -14,44 +14,45 @@ const btnOrder = document.querySelector(".order-project-btn");
 // console.log(navList);
 
 window.addEventListener("click", handlerCloseTabletMenu);
-window.addEventListener("scroll", () => {
+window.addEventListener("scroll", (evt) => {
 
-  if (window.scrollY >0) {
+  if (window.scrollY >0 ) {
       header.classList.add("active");
       btnOrder.classList.add('active')
-      navList.style.visibility = "hidden";
-    navList.style.opacity = "0";
+              navList.style.visibility = "hidden";
+              navList.style.opacity = "0";
   } else {
       header.classList.remove("active");
       btnOrder.classList.remove("active");
-    console.log("test");
+      console.log("test");
+
     }
 
+
 });
+
+
+
+
 
 // При клике на бургер-меню
+
 hamburger.addEventListener("click", function () {
-  console.log("test");
   // Добавляем или удаляем класс active для бургер-меню
-  hamburger.classList.toggle("active");
+    hamburger.classList.toggle("active");
+    if (hamburger.classList.contains('active')) { 
+        body.classList.toggle("body-no-scroll")
+                      navList.style.visibility = "visible";
+                      navList.style.opacity = "1";
+    }else{ 
+body.classList.toggle("body-no-scroll");
+    }
   // Добавляем или удаляем класс active для навигационного меню
-  navBar.classList.toggle("active");
+    navBar.classList.toggle("active");
 });
 
-// Добавляем и удаляем  класс по клику на меню на планшете
-// menuTablet.addEventListener("click", function (evt) {
-//              navBar.classList.toggle("active");
-//              hamburger.classList.toggle("active");
-//      console.log(evt.target);
 
-//     if (navBar.classList.contains('active')) {
-//         console.log("test");
-//         navList.style.visibility = "visible";
-//         navList.style.opacity = 1;
 
-//     }
-
-// });
 menuTablet.addEventListener("click", function (evt) {
   navBar.classList.toggle("active");
 
